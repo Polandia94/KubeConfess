28b5a867eb1102bccdf7247e1be0f33bf82219fe
arnavtripathy <arnavtripathy98@gmail.com>
2026-08-16 01:01:22 +0100

    Added config file

---
+++ config/vars.py
@@ -0,0 +1,7 @@
+import os
+
+API_KEY  = os.environ.get("API_KEY", "")
+BASE_URL = os.environ.get("BASE_URL", "https://api.anthropic.com/v1")
+MODEL_NAME = os.environ.get("MODEL_NAME", "claude-haiku-4-5")
+MAX_TOKENS = os.environ.get("MAX_TOKENS", 4096)
+
